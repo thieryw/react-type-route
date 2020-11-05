1f44ba9237ad1ed2a9928aa3816f3719a7c02fa1
William Thiery <williamalexthiery@hotmail.fr>
2020-11-05 17:34:03 +0100

    update

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,11 +1,134 @@
-import React from  "react";
+import React, {useRef, useCallback, useState} from  "react";
+import "./Home.scss";
+
+
 
 
 export const Home: React.FunctionComponent = ()=>{
 
+    const homeRef = useRef<HTMLDivElement>(null);
+    const headerRef = useRef<HTMLDivElement>(null);
+
+    const [backgroundPosition, setBackgroundPosition] = useState(-100);
+
+    const [currentScrollHeight, setCurrentScrollHeight] = useState(0);
+
+    const handleScroll = useCallback(async ()=>{
+
+        if(!homeRef || !homeRef.current){
+            return;
+        }
+
+
+        if(!headerRef || !headerRef.current){
+            return;
+        }
+
+           
+
+        headerRef.current.style.backgroundPositionY = `${backgroundPosition}px`
+
+
+        if(homeRef.current.scrollTop > currentScrollHeight){
+            setBackgroundPosition(backgroundPosition + 2);
+            console.log("down");
+        }else{
+
+            setBackgroundPosition(backgroundPosition - 2);
+
+            console.log("up");
+        }
+
+
+        setCurrentScrollHeight(homeRef.current.scrollTop);
+
+        
+
+    },[backgroundPosition, currentScrollHeight]);
+
+    
+
     return(
-        <div className="Home">
-            <h1>Home</h1>
+        <div onScroll={handleScroll} ref={homeRef} className="Home">
+            <header ref={headerRef}>
+
+                <h1>Home</h1>
+            </header>
+            <section>
+                <h2>Section</h2>
+                <p>
+                    lorem lipsum lorem lipsum lorem lipsum lorem lipsum lorem lipsum 
+                    lorem lipsum lorem lipsum lorem lipsum lorem lipsum lorem lipsum 
+                    lorem lipsum lorem lipsum lorem lipsum lorem lipsum lorem lipsum 
+                    lorem lipsum lorem lipsum lorem lipsum lorem lipsum lorem lipsum 
+                    lorem lipsum lorem lipsum lorem lipsum lorem lipsum lorem lipsum 
+                    lorem lipsum lorem lipsum lorem lipsum lorem lipsum lorem lipsum 
+                    lorem lipsum lorem lipsum lorem lipsum lorem lipsum lorem lipsum 
+                    lorem lipsum lorem lipsum lorem lipsum lorem lipsum lorem lipsum 
+                    lorem lipsum lorem lipsum lorem lipsum lorem lipsum lorem lipsum 
+                    lorem lipsum lorem lipsum lorem lipsum lorem lipsum lorem lipsum 
+                    lorem lipsum lorem lipsum lorem lipsum lorem lipsum lorem lipsum 
+                    lorem lipsum lorem lipsum lorem lipsum lorem lipsum lorem lipsum 
+                    lorem lipsum lorem lipsum lorem lipsum lorem lipsum lorem lipsum 
+                    lorem lipsum lorem lipsum lorem lipsum lorem lipsum lorem lipsum 
+                    lorem lipsum lorem lipsum lorem lipsum lorem lipsum lorem lipsum 
+                    lorem lipsum lorem lipsum lorem lipsum lorem lipsum lorem lipsum 
+                    lorem lipsum lorem lipsum lorem lipsum lorem lipsum lorem lipsum 
+                    lorem lipsum lorem lipsum lorem lipsum lorem lipsum lorem lipsum 
+                    lorem lipsum lorem lipsum lorem lipsum lorem lipsum lorem lipsum 
+                    lorem lipsum lorem lipsum lorem lipsum lorem lipsum lorem lipsum 
+                    lorem lipsum lorem lipsum lorem lipsum lorem lipsum lorem lipsum 
+                    lorem lipsum lorem lipsum lorem lipsum lorem lipsum lorem lipsum 
+                    lorem lipsum lorem lipsum lorem lipsum lorem lipsum lorem lipsum 
+                    lorem lipsum lorem lipsum lorem lipsum lorem lipsum lorem lipsum 
+                    lorem lipsum lorem lipsum lorem lipsum lorem lipsum lorem lipsum 
+                    lorem lipsum lorem lipsum lorem lipsum lorem lipsum lorem lipsum 
+                    lorem lipsum lorem lipsum lorem lipsum lorem lipsum lorem lipsum 
+                    lorem lipsum lorem lipsum lorem lipsum lorem lipsum lorem lipsum 
+                    lorem lipsum lorem lipsum lorem lipsum lorem lipsum lorem lipsum 
+                    lorem lipsum lorem lipsum lorem lipsum lorem lipsum lorem lipsum 
+                    lorem lipsum lorem lipsum lorem lipsum lorem lipsum lorem lipsum 
+                    lorem lipsum lorem lipsum lorem lipsum lorem lipsum lorem lipsum 
+                    lorem lipsum lorem lipsum lorem lipsum lorem lipsum lorem lipsum 
+                    lorem lipsum lorem lipsum lorem lipsum lorem lipsum lorem lipsum 
+                    lorem lipsum lorem lipsum lorem lipsum lorem lipsum lorem lipsum 
+                    lorem lipsum lorem lipsum lorem lipsum lorem lipsum lorem lipsum 
+                    lorem lipsum lorem lipsum lorem lipsum lorem lipsum lorem lipsum 
+                    lorem lipsum lorem lipsum lorem lipsum lorem lipsum lorem lipsum 
+                    lorem lipsum lorem lipsum lorem lipsum lorem lipsum lorem lipsum 
+                    lorem lipsum lorem lipsum lorem lipsum lorem lipsum lorem lipsum 
+                    lorem lipsum lorem lipsum lorem lipsum lorem lipsum lorem lipsum 
+                    lorem lipsum lorem lipsum lorem lipsum lorem lipsum lorem lipsum 
+                    lorem lipsum lorem lipsum lorem lipsum lorem lipsum lorem lipsum 
+                    lorem lipsum lorem lipsum lorem lipsum lorem lipsum lorem lipsum 
+                    lorem lipsum lorem lipsum lorem lipsum lorem lipsum lorem lipsum 
+                    lorem lipsum lorem lipsum lorem lipsum lorem lipsum lorem lipsum 
+                    lorem lipsum lorem lipsum lorem lipsum lorem lipsum lorem lipsum 
+                    lorem lipsum lorem lipsum lorem lipsum lorem lipsum lorem lipsum 
+                    lorem lipsum lorem lipsum lorem lipsum lorem lipsum lorem lipsum 
+                    lorem lipsum lorem lipsum lorem lipsum lorem lipsum lorem lipsum 
+                    lorem lipsum lorem lipsum lorem lipsum lorem lipsum lorem lipsum 
+                    lorem lipsum lorem lipsum lorem lipsum lorem lipsum lorem lipsum 
+                    lorem lipsum lorem lipsum lorem lipsum lorem lipsum lorem lipsum 
+                    lorem lipsum lorem lipsum lorem lipsum lorem lipsum lorem lipsum 
+                    lorem lipsum lorem lipsum lorem lipsum lorem lipsum lorem lipsum 
+                    lorem lipsum lorem lipsum lorem lipsum lorem lipsum lorem lipsum 
+                    lorem lipsum lorem lipsum lorem lipsum lorem lipsum lorem lipsum 
+                    lorem lipsum lorem lipsum lorem lipsum lorem lipsum lorem lipsum 
+                    lorem lipsum lorem lipsum lorem lipsum lorem lipsum lorem lipsum 
+                    lorem lipsum lorem lipsum lorem lipsum lorem lipsum lorem lipsum 
+                    lorem lipsum lorem lipsum lorem lipsum lorem lipsum lorem lipsum 
+                    lorem lipsum lorem lipsum lorem lipsum lorem lipsum lorem lipsum 
+                    lorem lipsum lorem lipsum lorem lipsum lorem lipsum lorem lipsum 
+                    lorem lipsum lorem lipsum lorem lipsum lorem lipsum lorem lipsum 
+                    lorem lipsum lorem lipsum lorem lipsum lorem lipsum lorem lipsum 
+                    lorem lipsum lorem lipsum lorem lipsum lorem lipsum lorem lipsum 
+                    lorem lipsum lorem lipsum lorem lipsum lorem lipsum lorem lipsum 
+                    lorem lipsum lorem lipsum lorem lipsum lorem lipsum lorem lipsum 
+                    lorem lipsum lorem lipsum lorem lipsum lorem lipsum lorem lipsum 
+                    lorem lipsum lorem lipsum lorem lipsum lorem lipsum lorem lipsum 
+                </p>
+            </section>
         </div>
     )
 }
